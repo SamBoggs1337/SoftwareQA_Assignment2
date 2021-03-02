1d99099913f215d987b68816043b6c66a1bf3069
Sam <sjb578@msstate.edu>
2021-03-02 17:33:08 -0600

    pushing initial retirement func

---
+++ retirement.h
@@ -0,0 +1,7 @@
+#ifndef RETIREMENT_H    // To make sure you don't declare the function more than once by including the header multiple times.
+#define RETIREMENT_H 
+
+int caclulateRetirement();
+
+
+#endif
