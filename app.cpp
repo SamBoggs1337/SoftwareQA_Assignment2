96f38bec4cd91db891a35a00110f20f49929ac02
Sam <sjb578@msstate.edu>
2021-03-02 17:32:16 -0600

    fixing conflicts

--- app.cpp
+++ app.cpp
@@ -1,54 +1,10 @@
 #include <iostream>
 #include <cstdlib>
 #include "BMI.h"
+#include "retirement.h"
+#include "UI.h"
 
 using namespace std;
-
-int caclulateRetirement()
-{
-    int age;
-    int salary;
-    float perSaved;
-    int desiredSavings;
-    int savingPerYear; // spy
-    int yearsToGoal;
-    int ageForGoal;
-
-    cout << "You Selected the Retirement Calculator, please fill out the following information! \n";
-    cout << "\n";
-    cout << "What is your current age?\n";
-    cin >> age;
-
-    cout << "\n";
-    cout << "What is your current annual salary?\n";
-    cin >> salary;
-    cout << "\n";
-
-    cout << "What is your percentage saved as a decimal?\n"; // + 35% employer too
-    cin >> perSaved;
-    cout << "What is your desired savings goal?\n";
-    cin >> desiredSavings;
-
-    savingPerYear = (salary * perSaved) * 1.35;
-    yearsToGoal = (desiredSavings / savingPerYear);
-    ageForGoal = age + yearsToGoal; // output; age for retirement goal
-
-}
-
-void displayMenu()
-{
-cout << "+-------------------------------------+" << endl;
-cout << "| Assignment 2 Software QA |" << endl;
-cout << "|=====================================|" << endl;
-cout << "| Select: |" << endl;
-cout << "| 1 => Caclulate your BMI |" << endl;
-cout << "| 2 => Calculate your retirement |" << endl;
-cout << "|-------------------------------------|" << endl;
-cout << "| Q => Quit |" << endl;
-cout << "+-------------------------------------+" << endl;
-cout << endl;
-cout << "Choice => ";
-}
 
 int main()
 {
